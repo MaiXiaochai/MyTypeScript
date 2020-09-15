ba0d70da976c3b8d317712f7b446ef67c55ec7fd
MaiXiaochai <maixiaochai@outlook.com>
2020-09-15 14:00:13 +0800

    *)试验一下后加的.gitignore文件是否对已经托管的文件起作用

---
+++ 1/HelloWorld.js
@@ -0,0 +1,12 @@
+/*
+* --------------------------------------
+* @File       : HelloWorld.ts
+* @Author     : maixiaochai
+* @Email      : maixiaochai@outlook.com
+* @CreatedOn  : 2020/9/15 11:29
+* --------------------------------------
+*/
+function log(msg) {
+    console.log('Hello, ' + msg);
+}
+log('Tom');
---
+++ 1/HelloWorld.ts
@@ -0,0 +1,17 @@
+/*
+* --------------------------------------
+* @File       : HelloWorld.ts
+* @Author     : maixiaochai
+* @Email      : maixiaochai@outlook.com
+* @CreatedOn  : 2020/9/15 11:29
+* --------------------------------------
+*/
+
+/*function log(msg) {
+    console.log('Hello, '+ msg)
+}
+
+log('Tom')*/
+
+
+// 接口
